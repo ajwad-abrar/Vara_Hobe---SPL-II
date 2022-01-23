34dad53af4edbace5c86c7fad3477369f7f0a5e6
ajwad-abrar <ajwadabrar@iut-dhaka.edu>
2022-01-23 22:48:40 +0600

    Added Varatia Home page

---
+++ varatia_home.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <!-- ===== BOX ICONS ===== -->
+        <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
+
+        <!-- ===== CSS ===== -->
+        <link rel="stylesheet" href="css/varatia_home_style.css">
+
+        <title>Varatia Home</title>
+    </head>
+    <body id="body-pd">
+        <header class="header" id="header">
+            <div class="header__toggle">
+                <i class='bx bx-menu' id="header-toggle"></i>
+            </div>
+
+            <div class="header__img">
+                <img src="img/demo.jpg" alt="">
+            </div>
+        </header>
+
+        <div class="l-navbar" id="nav-bar">
+            <nav class="nav">
+                <div>
+                    <a href="#" class="nav__logo">
+                        <i class='bx bx-layer nav__logo-icon'></i>
+                        <span class="nav__logo-name">Vara Hobe</span>
+                    </a>
+
+                    <div class="nav__list">
+                        <a href="#" class="nav__link active">
+                        <i class='bx bxs-home nav__icon' ></i>
+                            <span class="nav__name">Home</span>
+                        </a>
+
+                        <a href="varatia_profile.php" class="nav__link">
+                            <i class='bx bxs-user nav__icon' ></i>
+                            <span class="nav__name">Profile</span>
+                        </a>
+                        
+                        <a href="#" class="nav__link">
+                            <i class='bx bxs-search-alt-2 nav__icon' ></i>
+                            <span class="nav__name">Search Home</span>
+                        </a>
+
+                        <a href="#" class="nav__link">
+                            <i class='bx bxs-star-half nav__icon' ></i>
+                            <span class="nav__name">Review</span>
+                        </a>
+
+                        <!-- ei 2tar apatoto use nai -->
+
+                        <!-- <a href="#" class="nav__link">
+                            <i class='bx bx-folder nav__icon' ></i>
+                            <span class="nav__name">Data</span>
+                        </a>
+
+                        <a href="#" class="nav__link">
+                            <i class='bx bx-bar-chart-alt-2 nav__icon' ></i>
+                            <span class="nav__name">Analytics</span>
+                        </a> -->
+
+                        <!-- ei 2tar apatoto use nai -->
+
+                    </div>
+                </div>
+
+                <a href="#" class="nav__link">
+                    <i class='bx bx-log-out nav__icon' ></i>
+                    <span class="nav__name">Log Out</span>
+                </a>
+            </nav>
+        </div>
+
+       
+
+        <h1 class="text-center">Welcome home,  AJWAAAAAD Varatia</h1>
+        
+        <!--=====  JS =====-->
+        <script src="js/varatia_home_js.js"></script>
+    </body>
+</html>
