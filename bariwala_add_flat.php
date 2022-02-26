4198ec4eb442c56f099272ec1c79745d37e61641
ajwad-abrar <ajwadabrar@iut-dhaka.edu>
2022-02-26 16:47:32 +0600

    Added varatia_search_home.php page

---
+++ bariwala_add_flat.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <!-- ===== BOX ICONS ===== -->
+        <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
+
+        <!-- ===== CSS ===== -->
+        <link rel="stylesheet" href="css/bariwala_style.css">
+
+        <title>Bariwala Add Flat</title>
+    </head>
+    <body id="body-pd">
+        <header class="header" id="header">
+            <div class="header__toggle">
+                <i class='bx bx-menu' id="header-toggle"></i>
+            </div>
+
+            <div class="header__img">
+                <img src="img/demo.jpg" alt="">
+            </div>
+        </header>
+
+        <div class="l-navbar" id="nav-bar">
+            <nav class="nav">
+                <div>
+                    <a href="#" class="nav__logo">
+                        <i class='bx bx-layer nav__logo-icon'></i>
+                        <span class="nav__logo-name">Vara Hobe</span>
+                    </a>
+
+                    <div class="nav__list">
+                        <a href="bariwala_home.php" class="nav__link">
+                        <i class='bx bxs-home nav__icon' ></i>
+                            <span class="nav__name">Home</span>
+                        </a>
+
+                        <a href="bariwala_profile.php" class="nav__link">
+                            <i class='bx bxs-user nav__icon' ></i>
+                            <span class="nav__name">Profile</span>
+                        </a>
+
+                        <a href="bariwala_add_flat.php" class="nav__link active">
+                            <i class='bx bxs-folder-plus nav__icon' ></i>
+                            <span class="nav__name">Add Flat</span>
+                        </a>
+                        
+                        <a href="bariwala_flat_info.php" class="nav__link">
+                            <i class='bx bxs-home-heart nav__icon' ></i>
+                            <span class="nav__name">My Flat</span>
+                        </a>
+
+
+                    </div>
+                </div>
+
+                <a href="#" class="nav__link">
+                    <i class='bx bx-log-out nav__icon' ></i>
+                    <span class="nav__name">Log Out</span>
+                </a>
+            </nav>
+        </div>
+
+       
+
+
+
+    
+        
+        
+        <!--=====  JS =====-->
+        <script src="js/bariwala_js.js"></script>
+    </body>
+</html>
